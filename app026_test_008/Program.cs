313f94ae665d271c9e70555d87b72822af2431f8
Bogdan Kuznetsov <llgiant@gmail.com>
2019-06-02 19:42:37 +0600

    the true is out there

--- app026_test_008/Program.cs
+++ app026_test_008/Program.cs
@@ -2,18 +2,19 @@
 
 class Program
 {
-	static Random rnd = new Random();
+    static Random rnd = new Random();
 
-	static void Main()
-	{
-		Console.InputEncoding = System.Text.Encoding.UTF8;
-		Console.OutputEncoding = System.Text.Encoding.UTF8;
-		Console.WriteLine("========================================================================");
-		Console.WriteLine("Программа будет работать с текстом");
-		Console.WriteLine("========================================================================");
-		Console.WriteLine();
+    static void Main()
+    {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("========================================================================");
+        Console.WriteLine("Программа будет работать с текстом");
+        Console.WriteLine("========================================================================");
+        Console.WriteLine();
 
-		appBegin:
+    appBegin:
+        /*
          Задание №2
          * 1) Выписать через запятую все слова начинающиеся на букау "р"
          * 2) Выписать через запятую все слова оканчивающиеся на букау "а"
@@ -22,163 +23,163 @@ class Program
          * 5) Выписать через запятую количество символов между словами "Lorem"
          * 6) Выписсать построчно все предложения из текста.
          */
-		string text = "Lorem Ipsum д.н.э. - это текст-\"рыба\", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной \"рыбой\" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор слов, но это не совсем так. Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney. Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь. Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки, скрытой в середине абзаца. Также все другие известные генераторы Lorem Ipsum используют один и тот же текст, который они просто повторяют, пока не достигнут нужный объём. Это делает предлагаемый здесь генератор единственным настоящим Lorem Ipsum генератором.";
-		string endsWithA = "";
-		string startsWithP = "";
-		string toLowerText = text.ToLower();
-		int carret = 0;
-		string word = "";
-		int indexEndWord = 0;
-		Console.WriteLine(text);
-		Console.WriteLine("========================================================================");
-		while (true) // 1) Выписать через запятую все слова начинающиеся на букау "р"
-		{
+        string text = "Lorem Ipsum д.н.э. - это текст-\"рыба\", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной \"рыбой\" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор слов, но это не совсем так. Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney. Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда приемлемые модификации, например, юмористические вставки или слова, которые даже отдалённо не напоминают латынь. Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите какой-нибудь шутки, скрытой в середине абзаца. Также все другие известные генераторы Lorem Ipsum используют один и тот же текст, который они просто повторяют, пока не достигнут нужный объём. Это делает предлагаемый здесь генератор единственным настоящим Lorem Ipsum генератором.";
+        string endsWithA = "";
+        string startsWithP = "";
+        string toLowerText = text.ToLower();
+        int carret = 0;
+        string word = "";
+        int indexEndWord = 0;
+        Console.WriteLine(text);
+        Console.WriteLine("========================================================================");
+        while (true) // 1) Выписать через запятую все слова начинающиеся на букау "р"
+        {
 
-			carret = toLowerText.IndexOf("р", carret); // положение каретки
-			if (carret < 0) { break; }
-			if (carret == text.Length - 1) { break; }
+            carret = toLowerText.IndexOf("р", carret); // положение каретки
+            if (carret < 0) { break; }
+            if (carret == text.Length - 1) { break; }
 
-			if (carret > 0 && char.IsLetter(toLowerText[carret - 1]) && carret <= text.Length) { carret++; }
-			else
-			{
-				for (int index = carret + 1; index < text.Length; index++)
-				{
-					if (!char.IsLetter(toLowerText[index]))
-					{
-						indexEndWord = index;
-						break;
-					}
-				}
-				word = text.Substring(carret, indexEndWord - carret); // формирование слова
-				if (startsWithP.Length > 0) { startsWithP += ", "; }
-				startsWithP += word;
-				carret += word.Length;
-			}
-		}
-		carret = 0;
-		int indexStartWord = 0;
-		while (true)
-		{
-			carret = toLowerText.IndexOf("а", carret); // положение каретки
-			if (carret < 0 || carret == text.Length - 1) { break; }
-			if (carret > 0 && char.IsLetter(toLowerText[carret + 1]) && carret <= text.Length) { carret++; }
-			else
-			{
-				for (int index = carret - 1; index > 0; index--)
-				{
-					if (!char.IsLetter(toLowerText[index]))
-					{
-						indexStartWord = index;
-						break;
-					}
-				}
-				word = text.Substring(indexStartWord + 1, carret - indexStartWord); // формирование слова
-				if (endsWithA.Length > 0) { endsWithA += ", "; }
-				endsWithA += word;
-				carret += word.Length;
-			}
+            if (carret > 0 && char.IsLetter(toLowerText[carret - 1]) && carret <= text.Length) { carret++; }
+            else
+            {
+                for (int index = carret + 1; index < text.Length; index++)
+                {
+                    if (!char.IsLetter(toLowerText[index]))
+                    {
+                        indexEndWord = index;
+                        break;
+                    }
+                }
+                word = text.Substring(carret, indexEndWord - carret); // формирование слова
+                if (startsWithP.Length > 0) { startsWithP += ", "; }
+                startsWithP += word;
+                carret += word.Length;
+            }
+        }
+        carret = 0;
+        int indexStartWord = 0;
+        while (true)
+        {
+            carret = toLowerText.IndexOf("а", carret); // положение каретки
+            if (carret < 0 || carret == text.Length - 1) { break; }
+            if (carret > 0 && char.IsLetter(toLowerText[carret + 1]) && carret <= text.Length) { carret++; }
+            else
+            {
+                for (int index = carret - 1; index > 0; index--)
+                {
+                    if (!char.IsLetter(toLowerText[index]))
+                    {
+                        indexStartWord = index;
+                        break;
+                    }
+                }
+                word = text.Substring(indexStartWord + 1, carret - indexStartWord); // формирование слова
+                if (endsWithA.Length > 0) { endsWithA += ", "; }
+                endsWithA += word;
+                carret += word.Length;
+            }
 
-		}
-		carret = 0;
-		string containsPO = "";
-		int prevIndex = carret - 1;
-		int nextIndex = carret + 1;
-		int startIndex = 0;
-		int endIndex = text.Length;
-		word = "";
-		//text = "оларп оп";
-		int avWordLength = 0;
-		int countWords = 0;
-		int countSymbols = 0;
-		//найти слова в тексте которые содержат "О" и "П"
-		while (true)
-		{
-			carret = toLowerText.IndexOf("п", carret); // положение каретки
-			if (carret == -1) { break; }
+        }
+        carret = 0;
+        string containsPO = "";
+        int prevIndex = carret - 1;
+        int nextIndex = carret + 1;
+        int startIndex = 0;
+        int endIndex = text.Length;
+        word = "";
+        //text = "оларп оп";
+        int avWordLength = 0;
+        int countWords = 0;
+        int countSymbols = 0;
+        //найти слова в тексте которые содержат "О" и "П"
+        while (true)
+        {
+            carret = toLowerText.IndexOf("п", carret); // положение каретки
+            if (carret == -1) { break; }
 
-			for (int index = carret + 1; index < text.Length; index++)
-			{
-				if (!char.IsLetter(text[index]))
-				{
-					endIndex = index;
-					break;
-				}
-			}
-			for (int index = carret; index > 0; index--)
-			{
-				if (!char.IsLetter(text[index]))
-				{
-					startIndex = index + 1;
-					break;
-				}
-			}
-			word = toLowerText.Substring(startIndex, endIndex - startIndex);
-			avWordLength += endIndex - startIndex;
-			countWords++;
-			if (word.Contains("о"))
-			{
-				if (containsPO.Length > 0) { containsPO += ", "; }
-				containsPO += word;
-			}
-			carret = endIndex + 1;
-			if (carret >= text.Length) { break; }
-			startIndex = 0;
-			endIndex = text.Length;
-		} 
-		carret = 0;
-		int prevEnteance = toLowerText.IndexOf("lorem", carret);
-		endIndex = 0;
-		string strNumber = "";
-		while (true) // Выписать через запятую количество символов между словами "Lorem"
-		{
-			if (text.Length < 5) { break; }
-			carret = toLowerText.IndexOf("lorem", prevEnteance + 5); // положение каретки
-			if (carret < 0) { break; }
-			if (strNumber.Length > 0) { strNumber += ", "; }
-			strNumber += carret - (prevEnteance + 5);
+            for (int index = carret + 1; index < text.Length; index++)
+            {
+                if (!char.IsLetter(text[index]))
+                {
+                    endIndex = index;
+                    break;
+                }
+            }
+            for (int index = carret; index > 0; index--)
+            {
+                if (!char.IsLetter(text[index]))
+                {
+                    startIndex = index + 1;
+                    break;
+                }
+            }
+            word = toLowerText.Substring(startIndex, endIndex - startIndex);
+            avWordLength += endIndex - startIndex;
+            countWords++;
+            if (word.Contains("о"))
+            {
+                if (containsPO.Length > 0) { containsPO += ", "; }
+                containsPO += word;
+            }
+            carret = endIndex + 1;
+            if (carret >= text.Length) { break; }
+            startIndex = 0;
+            endIndex = text.Length;
+        }
+        carret = 0;
+        int prevEnteance = toLowerText.IndexOf("lorem", carret);
+        endIndex = 0;
+        string strNumber = "";
+        while (true) // Выписать через запятую количество символов между словами "Lorem"
+        {
+            if (text.Length < 5) { break; }
+            carret = toLowerText.IndexOf("lorem", prevEnteance + 5); // положение каретки
+            if (carret < 0) { break; }
+            if (strNumber.Length > 0) { strNumber += ", "; }
+            strNumber += carret - (prevEnteance + 5);
 
-			prevEnteance = carret;
+            prevEnteance = carret;
 
-		}
-		//Выписсать построчно все предложения из текста.
-		string strSentance = "";
-		startIndex = 0;
-		for (int index = 0; index < text.Length; index++)
-		{
-			if (text[index] == '.' || text[index] == '!' || text[index] == '?')
-			{
-				if (index != text.Length - 1)
-				{
-					if (!char.IsLetter(text[index + 1]) && !char.IsPunctuation(text[index + 1]) && char.IsUpper(text[index + 2]))
-					{
-						if (strSentance.Length > 0) { strSentance += "\n"; }
-						strSentance += text.Substring(startIndex, index - startIndex + 1);
-						startIndex = index + 2;
-					}
-				}
-				else
-				{
-					if (strSentance.Length > 0) { strSentance += "\n"; }
-					strSentance += text.Substring(startIndex, index - startIndex + 1);
-				}
-			}
-		} 
-		Console.WriteLine($"Cлова начинающиеся на букdу \"Р\": { startsWithP}");
-		Console.WriteLine($"Cлова оканчивающиеся на букву \"а\": { endsWithA}");
-		Console.WriteLine($"Cлова с буквами \"п\" и \"о\":{containsPO}");
-		Console.WriteLine($"Посчитать среднюю длину слов в тексте:{(double)avWordLength / countWords}");
-		Console.WriteLine($"Выписать через запятую количество символов между словами \"Lorem\":{strNumber}");
-		Console.WriteLine();
-		Console.WriteLine("Выписсать построчно все предлоожения из текста:");
-		Console.Write(strSentance);
+        }
+        //Выписсать построчно все предложения из текста.
+        string strSentance = "";
+        startIndex = 0;
+        for (int index = 0; index < text.Length; index++)
+        {
+            if (text[index] == '.' || text[index] == '!' || text[index] == '?')
+            {
+                if (index != text.Length - 1)
+                {
+                    if (!char.IsLetter(text[index + 1]) && !char.IsPunctuation(text[index + 1]) && char.IsUpper(text[index + 2]))
+                    {
+                        if (strSentance.Length > 0) { strSentance += "\n"; }
+                        strSentance += text.Substring(startIndex, index - startIndex + 1);
+                        startIndex = index + 2;
+                    }
+                }
+                else
+                {
+                    if (strSentance.Length > 0) { strSentance += "\n"; }
+                    strSentance += text.Substring(startIndex, index - startIndex + 1);
+                }
+            }
+        }
+        Console.WriteLine($"Cлова начинающиеся на букdу \"Р\": { startsWithP}");
+        Console.WriteLine($"Cлова оканчивающиеся на букву \"а\": { endsWithA}");
+        Console.WriteLine($"Cлова с буквами \"п\" и \"о\":{containsPO}");
+        Console.WriteLine($"Посчитать среднюю длину слов в тексте:{(double)avWordLength / countWords}");
+        Console.WriteLine($"Выписать через запятую количество символов между словами \"Lorem\":{strNumber}");
+        Console.WriteLine();
+        Console.WriteLine("Выписсать построчно все предлоожения из текста:");
+        Console.Write(strSentance);
 
-		appExit:
-		Console.WriteLine();
-		Console.WriteLine("========================================================================");
-		Console.WriteLine("Выйти из программы [y/n]?");
-		string eq = Console.ReadLine();
-		if (eq == "n" || eq == "N") { goto appBegin; }
-	}
+    appExit:
+        Console.WriteLine();
+        Console.WriteLine("========================================================================");
+        Console.WriteLine("Выйти из программы [y/n]?");
+        string eq = Console.ReadLine();
+        if (eq == "n" || eq == "N") { goto appBegin; }
+    }
 }
 
 
---
+++ palochki/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+class Program
+{
+    static Random rnd = new Random();
+    static void Main()
+    {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("========================================================================");
+        Console.WriteLine("Игра палочки");
+        Console.WriteLine("========================================================================");
+        Console.WriteLine();
+    appBegin:
+        char[] palochki = new char[] { '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|', '|' };
+        char[] input = new char[]    { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't' };
+        char[] inputCheck = new char[20];
+
+        Console.WriteLine("Выбирете игру:\n1 - играть против человека");
+        Console.WriteLine("2 - играть против машины");
+        int chooseGame = int.Parse(Console.ReadLine());
+        
+        if (chooseGame > 2 || chooseGame < 1) //Выбор игры против компьютера и человека
+        {
+            Console.WriteLine($"Такой игры нет в списке");
+            goto appBegin;
+        }
+        
+
+        if (chooseGame == 1) //Игра против человека
+        {
+            foreach (char letter in palochki) { Console.Write(letter + " "); }
+            Console.WriteLine();
+            foreach (char letter in input) { Console.Write(letter + " "); }
+
+
+
+        }
+        else  //Игра против компьютера
+        {
+
+
+
+
+        }
+    
+    appExit:
+        Console.WriteLine();
+        Console.WriteLine("========================================================================");
+        Console.WriteLine("Выйти из программы [y/n]?");
+        string eq = Console.ReadLine();
+        if (eq == "n" || eq == "N") { goto appBegin; }
+    }
+}
